720d72969d00c247e3f73e25fa9bfe5595d4640b
Rohit T P <tprohit9@gmail.com>
2022-05-30 22:41:17 +0530

    compressed

--- main.py
+++ main.py
@@ -5,68 +5,34 @@ import cv2
 import numpy as np
 import mediapipe as mp
 
-# import matplotlib.pyplot as plt
 
-mp_face_detection = mp.solutions.face_detection
-
-face_detection = mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5)
-
-img = cv2.imread("data/img_1.png")
-img = img[:, :, ::-1]
-img_cpy = img.copy()
-
-face_detection_results = face_detection.process(img)
-
-
-def _normalized_to_pixel_coordinates(
-        normalized_x: float, normalized_y: float, image_width: int,
-        image_height: int) -> Union[None, Tuple[int, int]]:
-
-    # Checks if the float value is between 0 and 1.
-    def is_valid_normalized_value(value: float) -> bool:
-        return (value > 0 or math.isclose(0, value)) and (value < 1 or
-                                                          math.isclose(1, value))
-
-    if not (is_valid_normalized_value(normalized_x) and
-            is_valid_normalized_value(normalized_y)):
-        return None
-
-    x_px = min(math.floor(normalized_x * image_width), image_width - 1)
-    y_px = min(math.floor(normalized_y * image_height), image_height - 1)
-    return x_px, y_px
+def to_pixel_coordinates(x: float, y: float, width: int, height: int) -> Union[None, Tuple[int, int]]:
+    return min(math.floor(x * width), width - 1), min(math.floor(y * height), height - 1)
 
 
 def get_rect(image: np.ndarray, detection):
-    image_rows, image_cols, _ = image.shape
-
-    location = detection.location_data
-
-    relative_bounding_box = location.relative_bounding_box
-    rect_start_point = _normalized_to_pixel_coordinates(
-        relative_bounding_box.xmin, relative_bounding_box.ymin, image_cols,
-        image_rows)
-    rect_end_point = _normalized_to_pixel_coordinates(
-        relative_bounding_box.xmin + relative_bounding_box.width,
-        relative_bounding_box.ymin + relative_bounding_box.height, image_cols,
-        image_rows)
-
-    return rect_start_point, rect_end_point
+    box = detection.location_data.relative_bounding_box
+    return [
+        to_pixel_coordinates(point[0], point[1], image.shape[1], image.shape[0])
+        for point in ((box.xmin, box.ymin), (box.xmin + box.width, box.ymin + box.height))
+    ]
 
 
-# print(face_detection_results.detections)
-if face_detection_results.detections:
+def main():
+    face_detection = mp.solutions.face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5)
+    img = cv2.imread("data/img_1.png")
 
-    for face_no, face in enumerate(face_detection_results.detections):
-        print(f'FACE NUMBER: {face_no + 1}')
+    face_detection_results = face_detection.process(img[:, :, ::-1])
 
-        box = face.location_data.relative_bounding_box
+    if not face_detection_results.detections:
+        return
 
+    for face in face_detection_results.detections:
         (x1, y1), (x2, y2) = get_rect(img, face)
 
-        print(box)
-        print((x1, y1), (x2, y2), img.shape)
-
-        # cv2.imshow("head", cv2.rectangle(img_cpy, (x1, y1), (x2, y2), color=(255,0,0)))
-        cv2.imshow("crop", img_cpy[y1:y2, x1:x2])
-        # cv2.imshow("box", )
+        cv2.imshow("crop", img[y1:y2, x1:x2])
         cv2.waitKey()
+
+
+if __name__ == "__main__":
+    main()
