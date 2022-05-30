7383d1bd79a212b3a0d670781277275be3cd6dd5
Rohit T P <tprohit9@gmail.com>
2022-05-30 23:53:23 +0530

    done ( actually done 😮)

--- main.py
+++ main.py
@@ -1,42 +1,36 @@
-import math
-from typing import Union, Tuple
-
 import cv2
-import numpy as np
-import mediapipe as mp
-
-
-def to_pixel_coordinates(x: float, y: float, width: int, height: int) -> Union[None, Tuple[int, int]]:
-    return min(math.floor(x * width), width - 1), min(math.floor(y * height), height - 1)
-
-
-def get_rect(image: np.ndarray, detection):
-    box = detection.location_data.relative_bounding_box
-    return [
-        to_pixel_coordinates(point[0], point[1], image.shape[1], image.shape[0])
-        for point in ((box.xmin, box.ymin), (box.xmin + box.width, box.ymin + box.height))
-    ]
-
-
-def get_faces():
-    faces = []
-    face_detection = mp.solutions.face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5)
-    img = cv2.imread("data/img_1.png")
-
-    face_detection_results = face_detection.process(img[:, :, ::-1])
-
-    if not face_detection_results.detections:
-        return
-
-    for face in face_detection_results.detections:
-        (x1, y1), (x2, y2) = get_rect(img, face)
-
-        faces.append(img[y1:y2, x1:x2])
-    return faces
+from fer import FER
 
 
 def main():
-    print(get_faces())
+    detector = FER(mtcnn=True)
+
+    emotion_peoples = {}
+    img = cv2.imread("data/img_1.png")
+
+    for emotions in detector.detect_emotions(img):
+        e_max, e_name = 0, ''
+
+        emotions = emotions['emotions']
+
+        for emotion in emotions:
+            if emotions[emotion] > e_max:
+                e_max, e_name = emotions[emotion], emotion
+
+        emotion_peoples[e_name] = emotion_peoples[e_name] + 1 if e_name in emotion_peoples else 1
+
+    e_max, e_name = 0, ''
+
+    print("\n\n-------------------------------------------------------------------\n\n")
+    print("There are ")
+
+    for emotion in emotion_peoples:
+        eno = emotion_peoples[emotion]
+        e_max, e_name = (eno, emotion) if eno > e_max else (e_max, e_name)
+
+        print(f"=> {eno} {'people' if eno > 1 else 'person'} feeling {emotion}")
+
+    print(f"\nOn average the photo looks {e_name}")
 
 
 if __name__ == "__main__":
